d016d594297559ade2ebd0ded276b05c783d46d2
Gerard Paapu <gerard@gerardpaapu.com>
2022-11-02 14:28:39 +1300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Write your code to complete the functions below.
+
+function concatMyArray(arrayOfStrings) {}
+
+function capitalize(sentence) {}
+
+function whereAreYou(arrayOfObjects) {}
+
+function deBee(string) {}
+
+module.exports = { concatMyArray, capitalize, whereAreYou, deBee }
---
+++ src/index.test.js
@@ -0,0 +1,97 @@
+const { concatMyArray, capitalize, whereAreYou, deBee } = require('./index')
+
+describe('concatMyArray', () => {
+  it('returns a string', () => {
+    expect(typeof concatMyArray(['hi'])).toBe('string')
+  })
+
+  it('returns a string where all the strings in the given array are joined together', () => {
+    expect(concatMyArray(['zoom', 'zoom', '!'])).toBe('zoomzoom!')
+  })
+
+  it('returns a string that matches the string given, but now joined together', () => {
+    expect(
+      concatMyArray(['super', 'cali', 'fragi', 'listic', 'expiali', 'docious'])
+    ).toBe('supercalifragilisticexpialidocious')
+  })
+})
+
+describe('capitalize', () => {
+  it('returns a string', () => {
+    expect(typeof capitalize('hello')).toBe('string')
+  })
+
+  it('should map over an array of words from the given string and then return a string where each of the words starts with a capital letter.', () => {
+    expect(capitalize('check check')).toBe('Check Check')
+  })
+
+  it('returns a string that matches the one it is given (but is now capitalized).', () => {
+    expect(capitalize('i can type many sentences')).toBe(
+      'I Can Type Many Sentences'
+    )
+  })
+})
+
+describe('whereAreYou', () => {
+  it('returns the first object where the value of hiding is "Scooby".', () => {
+    expect(
+      whereAreYou([
+        { doorNumber: 1, hiding: 'Shaggy' },
+        { doorNumber: 2, hiding: 'Scooby' },
+        { doorNumber: 3, hiding: 'The Monster!' },
+      ])
+    ).toStrictEqual({ doorNumber: 2, hiding: 'Scooby' })
+
+    expect(
+      whereAreYou([
+        { doorNumber: 1, hiding: 'Daphne' },
+        { doorNumber: 2, hiding: 'Shaggy' },
+        { doorNumber: 3, hiding: 'Velma' },
+        { doorNumber: 4, hiding: 'Fred' },
+        { doorNumber: 5, hiding: 'Scooby' },
+      ])
+    ).toStrictEqual({ doorNumber: 5, hiding: 'Scooby' })
+
+    expect(
+      whereAreYou([
+        { doorNumber: 4, hiding: 'Scooby' },
+        { doorNumber: 1, hiding: 'Scooby' },
+        { doorNumber: 3, hiding: 'Scooby' },
+        { doorNumber: 2, hiding: 'Scooby' },
+      ])
+    ).toStrictEqual({ doorNumber: 4, hiding: 'Scooby' })
+  })
+})
+
+describe('deBee', () => {
+  // {
+  //   actual: (typeof deBee('bizz buzz')),
+  //   expected: 'string',
+  //   message: "deBee should return a string."
+  // },
+
+  // {
+  //   actual: deBee("Help! buzz I'm buzz buzz surrounded buzz by buzz buzz bees!!"),
+  //   expected: "Help! I'm surrounded by bees!!",
+  //   message: "deBee should return a string where the word buzz has been removed."
+  // },
+
+  // {
+  //   actual: deBee('Buzz BUzz BuZZ help BUZZ buzz'),
+  //   expected: "help",
+  //   message: "deBee should "
+  // }
+  it('returns a string', () => {
+    expect(typeof deBee('bizz buzz')).toBe('string')
+  })
+
+  it('returns a string where the word buzz has been removed.', () => {
+    expect(
+      deBee("Help! buzz I'm buzz buzz surrounded buzz by buzz buzz bees!!")
+    ).toBe("Help! I'm surrounded by bees!!")
+  })
+
+  it('returns a string where the word buzz has been removed, regardless of capitalization.', () => {
+    expect(deBee('Buzz BUzz BuZZ help BUZZ buzz')).toBe('help')
+  })
+})
