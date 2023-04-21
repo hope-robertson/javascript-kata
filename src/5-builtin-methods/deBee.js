62f65cf058316523a824b2caf879aa15c9af4715
David Kavenga <95207521+david-kavenga@users.noreply.github.com>
2023-04-21 14:41:04 +1200

    Update deBee instructions

--- src/5-builtin-methods/deBee.js
+++ src/5-builtin-methods/deBee.js
@@ -1,6 +1,6 @@
 // This function takes a string. 
 // You will need to split the string into individual words before you filter out all those annoying buzzing sounds! 
-// It might be helpful to look at the tests in the deBee.test.js file or use console.log inside of the function.
+// It might be helpful to look at the deBee tests in the index.test.js file or use console.log inside of the function.
 
 function deBee(sentence) {
   
