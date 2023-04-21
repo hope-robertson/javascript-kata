cabae1a27ee07ea6ec11ce58c5f0f908275151a3
David Kavenga <95207521+david-kavenga@users.noreply.github.com>
2023-04-21 14:40:22 +1200

    Update whereAreYou instructions

--- src/5-builtin-methods/whereAreYou.js
+++ src/5-builtin-methods/whereAreYou.js
@@ -1,6 +1,6 @@
 // This function takes an array of objects. 
 // Use the .find() method and return the first object that “Scooby” is hiding within. 
-// Look at the tests in the whereAreYou.test.js file or use console.log inside of the function to determine the structure of objects in the array.
+// Look at the whereAreYou tests in the index.test.js file or use console.log inside of the function to determine the structure of objects in the array.
 
 function whereAreYou(arrayOfObjects) {
 
