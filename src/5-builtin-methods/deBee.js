be07202e4550b7ac5ec53b7990c4c467789a0002
Hope Robertson <hope.robertson.nz@gmail.com>
2024-04-03 15:36:28 +1300

    add completed code to correct repo

--- src/5-builtin-methods/deBee.js
+++ src/5-builtin-methods/deBee.js
@@ -2,8 +2,16 @@
 // You will need to split the string into individual words before you filter out all those annoying buzzing sounds!
 // It might be helpful to look at the deBee tests in the index.test.js file or use console.log inside of the function.
 
-function deBee(sentence) {}
+function deBee(sentence) {
+  // split the sentence into words
+  let words = sentence.split(' ')
+  // filter out instances of the word 'buzz', in a case-insensitive way so that all instances get picked up regardless of capitalisation
+  let filteredWords = words.filter((word) => word.toLowerCase() !== 'buzz')
+  // join the now filtered words into a sentence
+  let deeBeedSentence = filteredWords.join(' ')
+  return deeBeedSentence
+}
 
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
-export { deBee }
+module.exports = { deBee }
