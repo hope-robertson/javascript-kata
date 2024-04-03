eca2221d87ca6f0657d88b34fd8124a92f875e5b
Hope Robertson <hope.robertson.nz@gmail.com>
2024-04-03 16:06:10 +1300

    add code to correct repo

--- src/5-builtin-methods/whereAreYou.js
+++ src/5-builtin-methods/whereAreYou.js
@@ -2,8 +2,18 @@
 // Use the .find() method and return the first object that “Scooby” is hiding within.
 // Look at the whereAreYou tests in the index.test.js file or use console.log inside of the function to determine the structure of objects in the array.
 
-function whereAreYou(arrayOfObjects) {}
+// define function named whereAreYou which takes a single argument named arrayOfObjects
+function whereAreYou(arrayOfObjects) {
+  // use the find method to iterate through each object in the array and apply a callback function
+  let discoveredObject = arrayOfObjects.find(
+    // instructions for the callback function to check if the argument named 'hiding' within the current object has the value 'Scooby' using strict equality
+    (object) => object.hiding === 'Scooby' // if this condition is true for an object in the array, the method will stop iterating and return that object
+  )
+
+  // return the value of discoveredObject
+  return discoveredObject
+}
 
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
-export { whereAreYou }
+module.exports = { whereAreYou }
