1e70f1a2cfd9e4104f06b5e6d1d3f1697f48077a
Hope Robertson <hope.robertson.nz@gmail.com>
2024-04-03 16:37:15 +1300

    add correct code to the right repo

--- src/2-manipulating-objects/index.js
+++ src/2-manipulating-objects/index.js
@@ -9,28 +9,29 @@ let terah = {
 // write your code below this line---------------------------------
 
 // 1. Use object literal notation to assign adam the value of an object with no properties.
-let adam
+let adam = {
+  // 2. Give the adam object a name key that has the value "Adam".
+  name: 'Adam',
 
-// 2. Give the adam object a name key that has the value "Adam".
-
-// 3. Add a spouse key to the adam object and assign it the value of terah.
-
+  // 3. Add a spouse key to the adam object and assign it the value of terah.
+  spouse: terah, // add spouse key referencing terah object
+}
 // 4. Add a "spouse" key to terah and assign it the value of adam.
-
+terah.spouse = adam
 // 5. Change the value of the terah "age" property to 33.
-
+terah.age = 33 //Dot notation used to access the age.
 // 6. Remove the eyeColor property from terah (hint: use the delete operator).
-
+delete terah.eyeColor
 // 7. Add a children key to terah and use object literal notation to assign this variable to an empty object.
-
+terah.children = {}
 // 8. Add a ben property to the value of the terah children property. ben should be an object with the key name and the value "Ben".
-
+terah.children.ben = { name: 'Ben' }
 // 9. Add a wilson property to the value of the terah children property. wilson should be an object with the key name with the value "Wilson".
-
+terah.children.wilson = { name: 'Wilson' }
 // 10. Add a felicia property to the value of the terah children property. felicia should be an object with the key name with the value "Felicia".
-
+terah.children.felicia = { name: 'Felicia' }
 // 11. Add a children property to adam and assign it the value of terah.children.
-
+adam.children = terah.children
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
-export { terah, adam }
+module.exports = { terah, adam }
