c6591812c8e8275aa62a24fa396b49bf9a784348
Hope Robertson <hope.robertson.nz@gmail.com>
2024-04-03 16:36:16 +1300

    add code to correct repo

--- src/1-variables-and-arrays/index.js
+++ src/1-variables-and-arrays/index.js
@@ -5,9 +5,17 @@ let allowedIn
 let members
 
 // assign values to each of these variables to pass the tests. The test descriptions will guide you.
-secretNumber = 7
+
 // write your code below this line---------------------------------
+//assign a value to secretNumber, make it a number, make it exactly 7
+secretNumber = 7
+//assign a value to password, make it a string, make it a specific string
+password = 'just open the door'
+//assign a value to allowedIn, make it a boolean, make the boolean 'true'
+allowedIn = true
+//assign a value to members, make it an array, make the array start with the value 'Tui', make the array include 'Tane' at the 4th spot (i.e. members[3])
+members = ['Tui', 'Jonesy', 'Geezer', 'Tane']
 
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
-export { secretNumber, password, allowedIn, members }
+module.exports = { secretNumber, password, allowedIn, members }
