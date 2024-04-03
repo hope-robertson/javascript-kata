3d985a612750c0e9999f47423d311f396de628a1
Hope Robertson <hope.robertson.nz@gmail.com>
2024-04-03 14:52:19 +1300

    add completed concatMyArray challenge

--- src/5-builtin-methods/concatMyArray.js
+++ src/5-builtin-methods/concatMyArray.js
@@ -1,8 +1,12 @@
 // This function takes an array of individual strings
 // and then will need to .join() them together to return one string.
 
-function concatMyArray(arrayOfStrings) {}
-
+function concatMyArray(arrayOfStrings) {
+  let joinedString = arrayOfStrings.join('')
+  return joinedString
+}
+let arrayOfStrings = ['zoom', 'zoom', '!']
+let result = concatMyArray(arrayOfStrings) //this isn't passing, need help
 // write your code above this line----------------------------------
 // -----------------------------------------------------------------
-export { concatMyArray }
+module.exports = { concatMyArray }
